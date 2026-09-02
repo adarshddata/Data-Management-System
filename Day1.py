374d92e46e04aa1094750458c158e3cebb0b0c3b
adarshddata <adarshthapa007@gmail.com>
2026-09-02 20:01:53 +0545

    initial commit

---
+++ Day1.py
@@ -0,0 +1,265 @@
+# # print('hello world')
+# # print("adarsh")
+# # print("Hi im adarsh")
+# # print('hi ashmu')
+# # print('my ass is on fire')
+# # num1=input('Enter the num1')
+# # num2=input('Enter the num2')
+# # sum=num1+num2
+# # print('the sum is', sum)
+# # user=input('Enter the name')
+# # age =int(input('Enter the age'))
+# # print(f'hi user how are you and you are age old')
+
+# # user = input('Enter the name: ')
+# # age =int(input('Enter the age'))
+# # print(f'hi {user} how are you and you are {age} old')
+# # print(type(name))
+
+# # user=(input('Enter your name'))
+# # age=int(input('Enter your age'))
+# # print(f'hi {user}, how are you, and you are {age} years old')
+
+# # # print(type(name))
+
+# # # sum1=input(int('Enter the num1'))
+# # # num2=input(int('Enter the num2'))
+# # # sum = sum1+ sum2
+# # # print('the sum is', sum)
+# # num=input(int('Enter the number 1: '))
+# # num2=input(int('Enter the number 2: '))
+# # Total = num1 + num2
+# # print(Total)
+
+# # # name1='13%4 $ rohan aryal///__#'.lstrip('1234%$ ').rstrip('//_#') 
+# # # print(name1)
+
+# # # name2='     Rohan78Arya56 ////***----'.lstrip(' ' ).rstrip('56 /*-')
+# # # print(name2)
+# # # print(type(name2))
+# # # num3=56
+# # # print(type(num3))
+
+# # # user=input(('Entercyour name: '))
+# # # age=int(input('Enter your age: '))
+# # # print(f'Hi {user}, you are {age} years old')
+
+# # #data cleaning
+# # # name5='987%%%adarsh thapa --9/'.lstrip('987%')
+# # # print(name5)
+# # # #print(type(name5))
+# # # user=input('Your name: ')
+# # # age=int(input('Your age: '))   
+# # # print(f'({user} is {age} years old)')     
+
+# # name8=';;adarsh  thapa;"'.strip(';  "').replace('   ', '')
+# # print(name8)
+# # language='python java c'.replace('c', 'C').replace('p', 'P').replace('j', 'J')
+# # print(language)
+# # var='banana'.replace('a', 'A', 1)
+# # print(var)
+# # lang='Adarsh'.replace('A', 'a', 3)
+# # print(lang)
+
+# # print('adarsh')
+# # user=input('Your name: ')
+# # # age=int(input('Your age: '))
+# # # print(f'Hi {user}, aged {age}, please enter other details')
+
+# # # List= mutable; varied variables allowed
+# # # append=add variables
+# # # append, list , remove, slicing, reverse,
+
+# # L1=['rohit',1,34,76]
+# # print(L1)
+# # L1.append(100)
+# # print(L1)
+# # L1.reverse( )
+# # print(L1)
+# # #Tupple
+# # tup(1,2,3,'rohan',4)
+# # print(tup)
+
+# # tuple1=[1,2,6,8,2,1,0,0,1,6,5,8,5]
+# # distinct_count=len(set(tuple1))
+# # print(distinct_count)
+# # temp_list=list(1,2,6,8,2,1,0,0,1,6,5,8,5)
+# # temp_list.clear()
+# #print(temp_list)
+# # temp_list.extend([44,43])
+# tuple1=(1,2,4,5,7,8,2)
+# distinct_count=len(set(tuple1))
+# print(distinct_count)
+# temp_list=list([1,2,4,5,7,8,2])
+# temp_list.extend([22,87])
+# print(temp_list)
+
+# # num1=input('Enter the number: ')
+# # print(num1)
+# # if num1/2 is even:
+# # print(num1 'is even')
+
+# name= input('Name: ').strip()
+# print(name)
+# name=   adarsh.strip()
+# print(name)
+
+# name=(    'adarsh      thapa')
+# clean_name = ' '.join(name.split())
+# print(clean_name)
+
+# day='3'
+# match day:
+
+# switch1='a'
+# switch a= 
+
+# num1=int(input('enter number: '))
+# num2=int(input('enter the number: '))
+# op=input('enter the operation')
+# match op:
+# case 'add':
+#     print(num1+num2)
+# case 'sub':
+#         print(num1-num2)
+
+        # case 'multiply':
+        # print(num1*num2)
+
+        # case 'division':
+        # print(num1/num2)
+        # case '_':
+#         # print(invalid)
+
+        
+#         # pin=int(input('Enter your pin: '))
+#         # official_pin = '1989'
+#         # balance=5000
+#         # if pin=official_pin:
+#         # print('Pin verified succesfully')
+
+# #   official_pin ='1989'
+# #   balance = 5000
+# # pin=int(input('Enter your pin'))
+# # match pin:
+# # case 1989:
+# # print('Login successful')
+# # choice = input('Choose: balance, deposit, or withdraw: ')
+
+
+
+# # Get input and convert it to an integer
+# # pin = int(input("Enter your pin: "))
+# # match pin:
+# # case 1989:
+# #         print('access')
+
+# official_pin = 1989
+# balance = 5000
+# pin= (int(input('Enter your pin: '))
+# if pin == official_pin:
+# print('Login successful')
+# else:
+# print('Access denied')
+
+  
+#  official_pin = 1989
+#  balance = 5000
+# pin = int(input('Enter your pin: '))
+# if pin == official_pin:
+#   print('Login successful')
+# else:
+# #     print('Access denied')
+# official_pin = 1989
+# pin=int(input('Enter your pin: '))
+# if pin == official pin:
+#  print ('login succcesfull')
+#  else:
+#  print('Access denied')
+# official_pin=1989
+# balance=4000
+# pin=int(input('Enter your pin: '))
+#   if pin==official_pin:
+#     print('Login successful')
+#     if choice == 'balance':
+#     print(f'Your current balance is Rs.{balance}')
+#    else:
+#   print('Access denied')
+    official_pin= 1989
+   pin=int(input('Enter your pin: '))
+   if pin==official_pin:
+    print('Login successful')
+   else:
+    print('Access denied')
+
+ 
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+#     official_pin='1989'
+#     balance=5000
+
+#     pin=(input('Enter your pin: ')
+#     match pin:
+    
+#     case '1989':
+#     print('Login succesful')
+#     case _:
+#     print('Invalid pin')
+
+
+
+# #        else:
+# #        print('type verified pin; ')
+# # official_pin = 'a'
+    
+
+# official_pin = '1989'
+# pin = int(input('Enter your pin: '))
+# match pin:
+# case '1989':
+# print('Login successful')
+# case _:
+# print('Login unsuccesful')
+
+
+
+
+
+
+
+#         official_pin='a'
+#         match pin:
+#              case 'a': 
+#                   print('login succesful')
+#                   case_:
+#                   print('login unsuccesful')
+       
+
+
+
+
+
+
+
+
+        
+
+
+
+
---
+++ Leaf.py
@@ -0,0 +1,3 @@
+user =input('Enter your name: ')
+age=int(input('Enter your age: '))
+print(f'Hi You are {name}, and your age is {age}')
